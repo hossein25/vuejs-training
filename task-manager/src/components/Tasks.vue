c2df5cb3863abd4c167d75b8774752bb9c01aafa
hossein <hosseinmeysami.25@gmail.com>
2022-06-21 21:46:54 +0430

    create tasks component

---
+++ task-manager/src/components/Tasks.vue
@@ -0,0 +1,35 @@
+/* eslint-disable vue/multi-word-component-names */
+<template>
+  <div class="task-container">
+    <h1>Tasks</h1>
+    <ul>
+      <li :key="task.id" v-for="task in tasks">
+        <Task :task="task" :on-delete="onDelete" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Task from "./Task.vue";
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Tasks",
+  components: { Task },
+  props: {
+    tasks: Array,
+    onDelete: Function,
+  },
+};
+</script>
+
+<style scoped>
+.task-container {
+  padding: 2rem;
+  width: 400px;
+}
+li {
+  list-style: none;
+}
+</style>
